62ece06cf90339bd4501e81b1dc6aa6844c8b37c
jalurlangit-ops <lxgroup.kece@gmail.com>
2025-01-10 16:38:29 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,7 @@ function rank() {
 	"author": {
             "@type": "Person",
             "name": "Kstoto : Slot Deposit Pulsa 5000 Tanpa Potongan Dan Slot Gacor Terbaru 2025",
-            "reviewBody": "Kstoto : Slot Deposit Pulsa 5000 Tanpa Potongan Dan Slot Gacor Terbaru 2025, menawarkan pengalaman bermain slot gacor terbaik dengan keamanan terjamin. Nikmati pasaran Toto hongkong lotto lengkap dengan peluang menang tinggi, serta transaksi yang cepat dan aman. Menangkan hadiah besar di situs terpercaya ini!"
+            "reviewBody": "Kstoto menghadirkan pengalaman bermain slot yang menyenangkan dengan pilihan deposit pulsa mulai dari hanya 5000 tanpa potongan. Fitur ini memberikan kemudahan bagi para pemain untuk menikmati permainan slot tanpa harus khawatir dengan biaya tambahan atau proses transaksi yang rumit. Dengan deposit pulsa yang terjangkau"
           }
         },
     "offers": {
@@ -47,6 +47,22 @@ function rank() {
     };
   }
 
+function histats() {
+    return {
+      __html: `
+        var _Hasync= _Hasync|| [];
+        _Hasync.push(['Histats.start', '1,4921004,4,5,172,25,00010000']);
+        _Hasync.push(['Histats.fasi', '1']);
+        _Hasync.push(['Histats.track_hits', '']);
+        (function() {
+        var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
+        hs.src = ('//s10.histats.com/js15_as.js');
+        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+        })();
+      `,
+    };
+  };
+
 export default function RootLayout({
   children
 }: Readonly<{
@@ -55,7 +71,6 @@ export default function RootLayout({
   return (
     <html lang="id" data-browser="chrome">
       <head>
-        <meta charSet="utf-8" />
         <title>Kstoto : Slot Deposit Pulsa 5000 Tanpa Potongan Dan Slot Gacor Terbaru 2025</title>
         <meta name="description" content="Kstoto menghadirkan pengalaman bermain slot yang menyenangkan dengan pilihan deposit pulsa mulai dari hanya 5000 tanpa potongan. Fitur ini memberikan kemudahan bagi para pemain untuk menikmati permainan slot tanpa harus khawatir dengan biaya tambahan atau proses transaksi yang rumit. Dengan deposit pulsa yang terjangkau" />
         <meta name="keywords" content="Kstoto, Slot Deposit Pulsa 5000, Slot Gacor Terbaru 2025, Situs Slot Online, Situs Resmi Slot Online, Deposit Via Dana 5K, Daftar Slot Online, Slot Pulsa, Slot Gacor Gampang Menang, Slot Gacor 4D, Slot Pulsa Tanpa Potongan" />
@@ -80,7 +95,7 @@ export default function RootLayout({
         <meta name="distribution" content="global" />
         <meta name="format-detection" content="telephone=no, address=no, email=no" />
         <meta property="og:title" content="Kstoto : Slot Deposit Pulsa 5000 Tanpa Potongan Dan Slot Gacor Terbaru 2025" />
-        <meta property="og:description" content="Kstoto : Slot Deposit Pulsa 5000 Tanpa Potongan Dan Slot Gacor Terbaru 2025, menawarkan pengalaman bermain slot gacor terbaik dengan keamanan terjamin. Nikmati pasaran Toto hongkong lotto lengkap dengan peluang menang tinggi, serta transaksi yang cepat dan aman. Menangkan hadiah besar di situs terpercaya ini!" />
+        <meta property="og:description" content="Kstoto menghadirkan pengalaman bermain slot yang menyenangkan dengan pilihan deposit pulsa mulai dari hanya 5000 tanpa potongan. Fitur ini memberikan kemudahan bagi para pemain untuk menikmati permainan slot tanpa harus khawatir dengan biaya tambahan atau proses transaksi yang rumit. Dengan deposit pulsa yang terjangkau" />
         <meta property="og:url" content={nameDomain} />
         <meta property="og:site_name" content="kstoto" />
         <meta property="og:locale" content="id_ID" />
@@ -91,12 +106,12 @@ export default function RootLayout({
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:creator" content="kstoto" />
         <meta name="twitter:title" content="Kstoto : Slot Deposit Pulsa 5000 Tanpa Potongan Dan Slot Gacor Terbaru 2025" />
-        <meta name="twitter:description" content="Kstoto : Slot Deposit Pulsa 5000 Tanpa Potongan Dan Slot Gacor Terbaru 2025, menawarkan pengalaman bermain slot gacor terbaik dengan keamanan terjamin. Nikmati pasaran Toto hongkong lotto lengkap dengan peluang menang tinggi, serta transaksi yang cepat dan aman. Menangkan hadiah besar di situs terpercaya ini!" />
+        <meta name="twitter:description" content="Kstoto menghadirkan pengalaman bermain slot yang menyenangkan dengan pilihan deposit pulsa mulai dari hanya 5000 tanpa potongan. Fitur ini memberikan kemudahan bagi para pemain untuk menikmati permainan slot tanpa harus khawatir dengan biaya tambahan atau proses transaksi yang rumit. Dengan deposit pulsa yang terjangkau" />
         <meta name="twitter:image" content={nameDomain} />
         <link rel="icon" href="/favicon.ico?favicon.088e6c96.ico" sizes="256x256" type="image/x-icon" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="next-size-adjust" content="" />
         <script type="application/ld+json" dangerouslySetInnerHTML={rank()} key="product-jsonld" />
+        <script dangerouslySetInnerHTML={histats()} type="text/javascript" />
       </head>
       <body className={`${inter.variable} antialiased`}>{children}</body>
     </html>
