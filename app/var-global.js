b9b95e06723cb0c1dcc5e7cd2a7275abb44bb178
jalurlangit-ops <lxgroup.kece@gmail.com>
2025-01-20 21:24:18 +0700

    Update var-global.js

--- app/var-global.js
+++ app/var-global.js
@@ -1,1 +1,11 @@
-
+module.exports = {
+    title: "Kstoto : Slot Deposit Pulsa 5000 Tanpa Potongan Dan Slot Gacor Terbaru 2025",
+    desc: "Kstoto menghadirkan pengalaman bermain slot yang menyenangkan dengan pilihan deposit pulsa mulai dari hanya 5000 tanpa potongan. Fitur ini memberikan kemudahan bagi para pemain untuk menikmati permainan slot tanpa harus khawatir dengan biaya tambahan atau proses transaksi yang rumit. Dengan deposit pulsa yang terjangkau",
+    keywords: "Kstoto, Slot Deposit Pulsa 5000, Slot Gacor Terbaru 2025, Situs Slot Online, Situs Resmi Slot Online, Deposit Via Dana 5K, Daftar Slot Online, Slot Pulsa, Slot Gacor Gampang Menang, Slot Gacor 4D, Slot Pulsa Tanpa Potongan",
+    brandName: "KStoto",
+    domain: "https://jiggyhiphop.com",
+    histats: '1,4921004,4,5,172,25,00010000',
+    gsc: 'by3zYpiETgt5Dg5ASpsHoZ4lGQRWHmXxWiQCaxGP7tQ',
+    daftar: 'https://daftarwap.orang-dalam.link/ks',
+    login: 'https://daftarwap.orang-dalam.link/ks',
+}
